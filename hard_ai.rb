3a93f3ea3b4b1d739d804d858b99a126774300cd
ScStew <scottmstewart2@gmail.com>
2017-09-29 17:07:53 -0400

    added conditionals to test if computer or player has a possible win

--- hard_ai.rb
+++ hard_ai.rb
@@ -9,12 +9,17 @@ class Hard_ai
         board_class = Board.new
         player_class = Player.new
         open_spaces = []
+        win_move_arr = []
+        block_move_arr = 
         
         board.each_pair do |key,value|
             if key == value
                 open_spaces << key
             end
         end
-
+        open_spaces.each do |possible_choices|
+            board.update(player_class.player,possible_choices)
+            if board.winner? == true
+                win_move_arr << possible_choices
     end
 end
