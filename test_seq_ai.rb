d6de811d047e91ab8e87cccbdc60ca725e67763b
ScStew <scottmstewart2@gmail.com>
2017-09-29 00:32:50 -0400

    added test for last possible choice in sequence

--- test_seq_ai.rb
+++ test_seq_ai.rb
@@ -20,4 +20,10 @@ class Test_seq_ai < Minitest::Test
         board = {"1"=> "x","2" => "2","3"=> "3", "4"=> "4", "5" => "x" ,"6" => "6", "7" => "7","8" => "8", "9" => "9",}
         assert_equal("9",seq_ai.move(board))
     end
+
+    def test_move_last
+        seq_ai = Sequence_ai.new
+        board = {"1"=> "x","2" => "x","3"=> "x", "4"=> "x", "5" => "x" ,"6" => "x", "7" => "x","8" => "8", "9" => "x",}
+        assert_equal("8",seq_ai.move(board))
+    end
 end
