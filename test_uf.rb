164ec8648f2289117ce273cc635ec621c40a41ca
ScStew <scottmstewart2@gmail.com>
2017-10-04 10:32:00 -0400

    started test for winning_move

---
+++ test_uf.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "unbeatable_functions.rb"
+
+class Test_unbeatable < Minitest::Test
+    
+    def test_1_and_1
+        assert_equal(1,1)
+    end
+
+end
--- unbeatable_functions.rb
+++ unbeatable_functions.rb
@@ -1,7 +1,21 @@
 
+require_relative "board.rb"
 
-def winning_move(board)
+def winning_move(board,player)
+    board_class = Board.new
+    board_class.game_board = board
+    choice = ""
     board.each_pair do |key,value|
-        
-    
+        if key == value
+            key.update(player,key)
+            if board_class.winner? == true
+                choice = key
+            end
+            key.update(key,key)    
+        end
+    end
+    if choice == ""
+        choice = false
+    end
+    choice        
 end
