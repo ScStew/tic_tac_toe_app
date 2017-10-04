18fe144a2129d50d1af8e0136664a94ba3bcbce4
ScStew <scottmstewart2@gmail.com>
2017-10-04 11:58:35 -0400

    added blocking_move function

--- unbeatable_functions.rb
+++ unbeatable_functions.rb
@@ -1,5 +1,6 @@
 
 require_relative "board.rb"
+require_relative "player.rb"
 
 def winning_move(board,player)
     board_class = Board.new
@@ -19,3 +20,25 @@ def winning_move(board,player)
     end
     choice        
 end
+
+def blocking_move(board,player)
+    player_class = Player.new
+    player_class.player = player
+    board_class = Board.new
+    board_class.game_board = board
+    player = player_class.change_players
+    )
+    choice = ""
+    board.each_pair do |key,value|
+        if key == value
+            board_class.update(player,key)
+            if board_class.winner? == true
+                choice = key
+            end
+            board_class.update(key,key)    
+        end
+    end
+    if choice == ""
+        choice = false
+    end
+    choice       
