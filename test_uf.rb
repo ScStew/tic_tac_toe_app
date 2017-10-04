e5551b7111199fe7e2e8c37719e66256c1ccdf21
ScStew <scottmstewart2@gmail.com>
2017-10-04 10:52:46 -0400

    added test for winning move to test rows,columns,and diaganol

--- test_uf.rb
+++ test_uf.rb
@@ -13,9 +13,22 @@ class Test_unbeatable < Minitest::Test
         assert_equal(false,winning_move(board,player))
     end
 
-    def test_winning_move_1
+    def test_winning_move_1_row
         board = {"1"=> "1","2" => "x","3"=> "x", "4"=> "o", "5" => "5" ,"6" => "6", "7" => "7","8" => "8", "9" => "9"}
         player = "x"
         assert_equal("1",winning_move(board,player))
     end
+
+    def test_winning_move_2_column
+        board = {"1"=> "1","2" => "2","3"=> "3", "4"=> "o", "5" => "x" ,"6" => "o", "7" => "7","8" => "x", "9" => "9"}
+        player = "x"
+        assert_equal("2",winning_move(board,player))
+    end
+
+    def test_winning_move_3_diagonal
+        board = {"1"=> "1","2" => "o","3"=> "3", "4"=> "o", "5" => "x" ,"6" => "6", "7" => "x","8" => "8", "9" => ""}
+        player = "x"
+        assert_equal('3',winning_move(board,player))
+    end
+
 end
