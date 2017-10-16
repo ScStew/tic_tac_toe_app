b43c4eb10bd77f2ff1d2f930c55cf6a7883dfdf9
ScStew <scottmstewart2@gmail.com>
2017-10-16 15:55:24 -0400

    made function for pulling all values for database table

--- db.rb
+++ db.rb
@@ -3,7 +3,7 @@ load './local_env.rb' if File.exist?('./local_env.rb')
 
 
 def winner_card(players,message)
-p message
+ time = Time.new
     db_params = {
         host: ENV['host'],
         port: ENV['port'],
@@ -14,5 +14,22 @@ p message
     db = PG::Connection.new(db_params)
 
     # db.exec("UPDATE winning_card SET player_1 = '#{players[0]}',player_2 = '#{players[1]}', outcome = '#{message}'")
-    db.exec("insert into winning_card(player_1,player_2,outcome,date_time)VALUES('#{players[0]}','#{players[1]}','#{message}', 'no time yet')")    
+    db.exec("insert into winning_card(player_1,player_2,outcome,date_time)VALUES('#{players[0]}','#{players[1]}','#{message}', '#{time}')")    
 end
+
+def pull_table()
+    db_params = {
+        host: ENV['host'],
+        port: ENV['port'],
+        dbname: ENV['dbname'],
+        user: ENV['user'],
+        password: ENV['password']
+    }
+
+    db = PG::Connection.new(db_params)
+
+    table = db.exec("select * from winning_card")
+    table.values
+end
+
+pull_table()
